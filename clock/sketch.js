128404bb4fdf8c539e2c2722f36a3771fced38f7
yuki <yuki@example.com>
2026-06-02 13:10:18 +0900

    clock

---
+++ clock/sketch.js
@@ -0,0 +1,24 @@
+
+
+function setup(){
+  createCanvas(windowWidth,windowHeight)
+
+
+
+}
+
+function draw(){
+  background(220)
+
+  fill(0,0,0)
+  textFont('Georgia')
+textSize(100)
+textAlign(CENTER)
+  text("12:28",width/2,height/2)
+
+  noFill()
+  circle(width/2,height/2,400)
+}
+
+
+
