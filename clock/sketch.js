2357103e06623860940ee92951e7fba2f0d672f3
yuki <yuki@example.com>
2026-07-28 15:36:37 +0900

    tokei updata

--- clock/sketch.js
+++ clock/sketch.js
@@ -43,7 +43,11 @@ function receive(data) {
 }
 
 function draw() {
+  background(220);
+  
   let date = new Date()
+
+  console.log(date.getSeconds());
 
   let h = date.getHours()
   let m = date.getMinutes()
@@ -52,7 +56,7 @@ function draw() {
   // background(map(24, 0, 24, 0, 255))
 
   if (sky) {
-    background(sky.get(map(h, 0, 24, 0, 1000), 0))
+    //background(sky.get(map(h, 0, 24, 0, 1000), 0))
 
 
   }
@@ -64,6 +68,95 @@ function draw() {
   push();
   translate(width / 2, height / 2)
 
+  // 外枠
+noFill();
+stroke(70);
+strokeWeight(12);
+circle(0, 0, 570);
+
+// 内側の枠
+stroke(180);
+strokeWeight(4);
+circle(0, 0, 550);
+
+for(let i = 0; i < 60; i++){
+
+    let angle = i * 6 - 90;
+
+    let r1 = 225;
+    let r2 = 240;
+
+    if(i % 5 == 0){
+        // 5分ごとの長い線
+        strokeWeight(5);
+        r1 = 210;
+    }else{
+        // 普通の線
+        strokeWeight(2);
+    }
+
+    line(
+        cos(angle) * r1,
+        sin(angle) * r1,
+        cos(angle) * r2,
+        sin(angle) * r2
+    );
+}
+
+
+fill(80);
+stroke(255);
+strokeWeight(2);
+
+circle(0,0,18);
+
+fill(200);
+circle(0,0,8);
+
+  // 時針
+stroke(0);
+strokeWeight(8);
+
+let hourAngle = (h % 12) * 30 + m * 0.5 - 90;
+
+line(
+  0,
+  0,
+  cos(hourAngle) * 120,
+  sin(hourAngle) * 120
+);
+
+// 分針
+stroke(50);
+strokeWeight(5);
+
+let minuteAngle = m * 6 + s * 0.1 - 90;
+
+line(
+  0,
+  0,
+  cos(minuteAngle) * 180,
+  sin(minuteAngle) * 180
+);
+
+// 秒針
+stroke("red");
+strokeWeight(2);
+
+let secondAngle = s * 6 - 90;
+
+line(
+  0,
+  0,
+  cos(secondAngle) * 220,
+  sin(secondAngle) * 220
+);
+
+// 中心の丸
+fill(0);
+noStroke();
+circle(0, 0, 15);
+
   for (let i = 1; i <= 12; i++) {
     let r = 250;//半径
     let d = (i * 30) - 90;//角度
@@ -72,10 +165,7 @@ function draw() {
     text(i, x, y)//数字を描画
   }
 
-  //分針
-  strokeWeight(6)
-  stroke('red')
-  line(0, 0, cos(6 * m - 90) * 220, sin(6 * m - 90) * 220)
+
 
   if (bg) {
     bg.resize(400, 0)
@@ -88,13 +178,13 @@ function draw() {
   textAlign(CENTER)
 
   text(date.getFullYear(), 0, -300)
-  text(date.getHours() + ":" + date.getMinutes(), 0, 0)
-  text(date.getSeconds() + "." + date.getMilliseconds(), 0, 100)
+  text(date.getHours() + ":" + nf(date.getMinutes(), 2), -450, -200);
+  text(date.getSeconds(), -450, -100);
 
   if (tenki) {
-    text(tenki.hourly.temperature_2m[date.getDate.Hours()] + "°C", 0, 200)
+    text(tenki.hourly.temperature_2m[h] + "°C", -450, 0)
 
-    let wc = temki.hourly.weather_code[h];
+    let wc = tenki.hourly.weather_code[h];
     wc = 0; // デバッグ用。あとで消すこと
     if (wc == 0) {
       // 快晴
@@ -105,14 +195,14 @@ function draw() {
     } else {
       // それ以外
       wc = floor(wc / 10); // 10 の位（小数点切り捨て）
-      if (4) {
+      if (wc ==4) {
         // 霧
 
-      } else if (7) {
+      } else if (wc ==7) {
         // 雪
-      } else if (6){
+      } else if (wc ==6){
         //雨
-      }else if (9){
+      }else if (wc ==9){
         //雷
       }
     }
@@ -125,10 +215,10 @@ function draw() {
 
 
 
-  beginClip()
-  noFill()
-  circle(width / 2, height / 2, 400)
-  endClip()
+ // beginClip()
+ // noFill()
+  //circle(width / 2, height / 2, 400)
+  //endClip()
 
 }
 
